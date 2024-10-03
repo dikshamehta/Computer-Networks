b4dbe5ce14f7360eea80d02a9cc3c5ff45f0be66
dimehta <dikshamehta4214t@gmail.com>
2024-10-02 23:15:40 -0400

    Added comments RUDP implementation in Transport

--- src/sans_transport.c
+++ src/sans_transport.c
@@ -1,44 +1,135 @@
 #include <sys/socket.h>
-#include <errno.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdlib.h>
+#include "include/sans.h"
+#include "include/rudp.h"
 #include <stdio.h>
 
-int sans_send_pkt(int socket, const char *buf, int len)
-{
-    printf("sans_send_pkt\n");
-    if (buf == NULL || len <= 0)
-    {
-        errno = EINVAL;
+typedef struct connection_rudp_t {
+    int sfd;
+    struct sockaddr_in addr;
+    size_t seq_num;
+} connection_rudp_t;
+
+extern connection_rudp_t connection_rudp;
+
+//handling Timeout
+static int configure_socket_timeout(int socket, long timeout_sec, long timeout_usec) {
+    struct timeval timeout = {
+            .tv_sec = timeout_sec,
+            .tv_usec = timeout_usec
+    };
+    if (setsockopt(socket, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) < 0) {
+        printf("setsockopt failed\n");
         return -1;
     }
-
-    ssize_t bytes_sent = sendto(socket, buf, len, 0, NULL, 0);
-
-    if (bytes_sent < 0)
-    {
-        // Error occurred, errno is set by sendto
-        return -1;
-    }
-
-    return (int)bytes_sent;
+    return 0;
 }
 
-int sans_recv_pkt(int socket, char *buf, int len)
-{
-    printf("sans_recv_pkt\n");
 
-    if (buf == NULL || len <= 0)
-    {
-        errno = EINVAL;
+static int helper_send_packet(int socket, rudp_packet_t *packet) {
+    if (sendto(socket, packet, sizeof(*packet), 0,
+               (struct sockaddr*)&connection_rudp.addr, sizeof(connection_rudp.addr)) < 0) {
+        printf("sendto failed\n");
+        return -1;
+    }
+    return 0;
+}
+
+static int helper_receive_packet(int socket, rudp_packet_t *packet) {
+    socklen_t addr_len = sizeof(connection_rudp.addr);
+    int bytes_received = recvfrom(socket, packet, sizeof(*packet), 0,
+                                  (struct sockaddr*)&connection_rudp.addr, &addr_len);
+    return bytes_received;
+}
+
+int sans_send_pkt(int socket, const char* buf, int len) {
+    if (buf == NULL || len <= 0) return -1;
+
+    if (configure_socket_timeout(socket, 0, 200000) < 0) {
         return -1;
     }
 
-    ssize_t bytes_received = recvfrom(socket, buf, len, 0, NULL, NULL);
+    // Generating Data Packet
+    rudp_packet_t data_packet = { .type = DAT };
 
-    if (bytes_received < 0)
-    {
-        // Error occurred, errno is set by recvfrom
-        return -1;
+    // calculating total number of packets which can be sent full
+    size_t total_full_packets = len / SIZE_OF_PAYLOAD;
+
+    for (int i = 0; i <= total_full_packets; ++i) {
+        // calculating the offset to find the memory location of data
+        size_t buffer_offset = i * SIZE_OF_PAYLOAD;
+        size_t size_of_payload = SIZE_OF_PAYLOAD;
+
+        size_t curr_seq_num = connection_rudp.seq_num + i;
+        data_packet.seqnum = curr_seq_num;
+
+        //handlign the last packet which might not be full
+        if (i == total_full_packets) {
+            if (len % SIZE_OF_PAYLOAD == 0) break; // If all the packet were of full size
+            size_of_payload = len % SIZE_OF_PAYLOAD;  // last packet which might not be full
+        }
+        memcpy(data_packet.payload, buf + buffer_offset, size_of_payload);
+
+        rudp_packet_t ack_pkt;
+        while (1) {
+            if (helper_send_packet(socket, &data_packet) < 0) return -1;
+
+            int ack_res = helper_receive_packet(socket, &ack_pkt);
+            if (ack_res > 0 && ack_pkt.type == ACK && ack_pkt.seqnum == curr_seq_num) {
+                connection_rudp.seq_num = curr_seq_num + 1;
+                break;
+            }
+        }
     }
 
-    return (int)bytes_received;
+    return len;
+}
+
+// Function to receive data
+int sans_recv_pkt(int socket, char* buf, int len) {
+    if (buf == NULL || len <= 0) return -1;
+
+    // Generating recieve Packet and acknolowdgement
+    rudp_packet_t packet_rcv;
+    rudp_packet_t packet_ack = { .type = ACK };
+
+    // calculating total number of packets which can be sent full
+    size_t total_full_packets = len / SIZE_OF_PAYLOAD;
+
+
+    for (int i = 0; i <= total_full_packets; ++i) {
+        // calculating the offset to find the memory location of data
+        size_t buffer_offset = i * SIZE_OF_PAYLOAD;
+        size_t size_of_payload = SIZE_OF_PAYLOAD;
+
+        size_t real_seq_num = connection_rudp.seq_num + i;
+
+        //handlign the last packet which might not be full
+        if (i == total_full_packets) {
+            if (len % SIZE_OF_PAYLOAD == 0) break; // If all the packet were of full size
+            size_of_payload = len % SIZE_OF_PAYLOAD;  // last packet which might not be full
+        }
+
+        while (1) {
+            int n_recvd_bytes = helper_receive_packet(socket, &packet_rcv);
+            if (n_recvd_bytes > 0 && packet_rcv.type == DAT) {
+                if (packet_rcv.seqnum == real_seq_num) {
+                    memcpy(buf + buffer_offset, packet_rcv.payload, size_of_payload);
+
+                    packet_ack.seqnum = real_seq_num;
+                    if (helper_send_packet(socket, &packet_ack) < 0) {
+                        return -1;
+                    }
+
+                    connection_rudp.seq_num = real_seq_num + 1;
+                    break;
+                }
+
+            }
+        }
+    }
+
+    return len;
 }
